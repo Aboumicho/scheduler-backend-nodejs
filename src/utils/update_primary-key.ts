2c10dd92d18eb3996119ebd1b7acb65b5c1578ae
Michel Maroun <michoma94@outlook.com>
2024-06-06 17:09:14 -0400

    Update update_primary-key.ts

--- src/utils/update_primary-key.ts
+++ src/utils/update_primary-key.ts
@@ -2,7 +2,7 @@ import Counter from "models/counter";
 
 export const updatePrimaryKey = async (id: string) => {
     return Counter.findByIdAndUpdate(
-        { _id: 'serviceId' },
+        { _id: id },
         { $inc: { seq: 1 } },
         { new: true, upsert: true }
     );
